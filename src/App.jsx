55ffc246c3f4cba6174a9a5c1b689f7b6ca1e0c6
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2023-10-19 20:54:11 +0100

    make page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+import background from './assets/background.jpg';
+import logo from './assets/logo.png'
 function App() {
     return (
-        <>
-            <h1 className=' text-4xl'>Hello </h1>
-        </>
+        <div className="h-screen relative" id='container'>
+            <img src={background} className=' w-full h-full object-cover object-[60%] absolute z-[-1]' alt="" />
+            <div className='w-full h-full bg-black absolute opacity-50  z-[-1]'></div>
+
+            <div className=' flex justify-center flex-col items-center'>
+                <div className='bg-white m-4 rounded-xl'>
+                    <img src={logo} className='w-28 h-auto m-4' alt="" />
+                </div>
+
+                <h3 className=' font-jetbrains text-slate-200'>Portfolio in progress</h3>
+            </div>
+        </div>
     )
 }
 
