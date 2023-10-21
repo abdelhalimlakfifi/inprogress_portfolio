c473505ae59309294085dc555a9068486416ce8d
abdelhalimlakfifi <abdelhalimlakfifi@gmail.com>
2023-10-21 23:56:21 +0100

    make desktop design

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@
 import './App.css'
 import background from './assets/background.jpg';
 import logo from './assets/logo.png'
+import Desktop from './devices/Desktop';
 import Mobile from './devices/Mobile';
 import Tablet from './devices/Tablet';
 
 function App() {
     return (
 
-        <Tablet />
 
+        <Desktop />
+        
+        // <Tablet />
         // <Mobile />
     )
 }
